6a2ba50cf4bce78f7bd9bc623700ddeb6aa0add2
yanuarfrd <yanuarfrds@365.telkomuniversity.ac.id>
2022-04-09 10:37:49 +0700

    Awallan

---
+++ Modul7_1302204001/BankTransferConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul7_1302204001
+{
+    internal class Class1
+    {
+    }
+}
